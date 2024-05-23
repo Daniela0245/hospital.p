65213719247ed7c5c4824db3884444e4e927774f
Daniela <danielaquinones.1203@gmail.com>
2024-05-22 21:28:08 -0500

    Ultimos cambios

--- src/home/Home.vue
+++ src/home/Home.vue
@@ -4,7 +4,7 @@
       <h2>Bienvenido a la Administración para Hospitales</h2>
       <p>Con nuestra aplicación, podrás llevar a cabo todas las operaciones necesarias para la gestión eficiente de tu centro médico. Desde la administración de pacientes hasta la coordinación del personal médico, pasando por la gestión de citas médicas, tratamientos y facturación, todo está cubierto.</p>
       <p>ECon un diseño intuitivo y amigable, podrás acceder y gestionar la información de pacientes, programar citas médicas de manera eficiente, asignar tratamientos específicos y llevar un seguimiento detallado de la facturación. Además, nuestro sistema está diseñado para proporcionar una experiencia fluida y segura, garantizando la confidencialidad y privacidad de los datos sensibles de los pacientes y la información crítica del hospital.</p>
-      <p> Ya sea que estés administrando un pequeño consultorio médico o una gran institución hospitalaria, nuestro sistema está diseñado para adaptarse </p>
+      <p>Ya sea que estés administrando un pequeño consultorio médico o una gran institución hospitalaria, nuestro sistema está diseñado para adaptarse a tus necesidades específicas y simplificar tus procesos administrativos. Únete a nosotros y descubre cómo nuestro sistema de administración hospitalaria puede optimizar la eficiencia y mejorar la atención médica en tu institución."</p>
 
       <!-- Aquí puedes agregar más contenido e información relevante -->
     </div>
